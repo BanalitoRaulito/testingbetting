afd60377b140968f3c230a811d9074d44bb6eb71
BanalitoRaulito <raul.volmar@gmail.com>
2020-06-11 19:58:39 +0300

    improving testing

--- src/test.js
+++ src/test.js
@@ -15,16 +15,8 @@ var makeLobby = () => {
   return new Lobby(socket, data, searching, teams)
 }
 
+
 // test 1
-test("matchTeam 1 player connect", () => {
-  let l = makeLobby()
-
-  matchTeam(l.socket, l.data, l.searching, l.teams)
-  l.expectWithOne()
-})
-
-
-// test 2
 test("matchTeam 1 player connects and then cancels", () => {
   let l = makeLobby()
 
@@ -36,8 +28,8 @@ test("matchTeam 1 player connects and then cancels", () => {
 })
 
 
-// test 3
-test("matchTeam 2 player connect", () => {
+// test 2
+test("matchTeam 4 player connect", () => {
   let l = makeLobby()
 
   // 1
@@ -48,4 +40,14 @@ test("matchTeam 2 player connect", () => {
   l.data.address = "TEL1u2FV3yTh6iiPFC3KCXFzeXbnU7SxLR"
   matchTeam(l.socket, l.data, l.searching, l.teams)
   l.expectWithTwo()
+
+  // 3
+  l.data.address = "TBzs1vEYLkWRasgrt9r6CfzC1tvxuLv8GZ"
+  matchTeam(l.socket, l.data, l.searching, l.teams)
+  l.expectWithThree()
+
+  // 4
+  l.data.address = "TNDd8X6vH16ougLhtUjPNeXPT5rkAuCp2m"
+  matchTeam(l.socket, l.data, l.searching, l.teams)
+  l.expectWithFour()
 })
--- src/test_lobby.js
+++ src/test_lobby.js
@@ -1,3 +1,10 @@
+var defaultMSG_grup = [
+  ["msg", { "msg": "searching" }],
+  ["msg", { "msg": "searching" }],
+  ["msg", { "msg": "acceptNow" }],
+  ["msg", { "msg": "acceptNow" }]
+]
+
 module.exports = class Lobby{
   constructor(socket, data, searching, teams){
     this.socket = socket;
@@ -6,7 +13,17 @@ module.exports = class Lobby{
     this.teams = teams;
   }
 
+  //cancel
+  expectWithCancel(){
+    expect(this.socket.emit.mock.calls.length).toBe(2)
+    expect(this.socket.emit.mock.calls).toEqual([
+      ["msg", { "msg": "searching" }],
+      ["msg", { "msg": "cancel" }]
+    ])
+    expect(this.searching.length).toBe(0)
+  }
 
+  // 1
   expectWithOne(){
     expect(this.socket.emit.mock.calls.length).toBe(1)
     expect(this.socket.emit.mock.calls[0]).toEqual([
@@ -19,26 +36,31 @@ module.exports = class Lobby{
     expect(this.teams).toEqual([])
   }
 
-
+  // 2
   expectWithTwo(){
     expect(this.socket.emit.mock.calls.length).toBe(4)
-    expect(this.socket.emit.mock.calls).toEqual([
-      ["msg", { "msg": "searching" }],
-      ["msg", { "msg": "searching" }],
-      ["msg", { "msg": "acceptNow" }],
-      ["msg", { "msg": "acceptNow" }]
-    ])
+    expect(this.socket.emit.mock.calls).toEqual(defaultMSG_grup)
     expect(this.searching.length).toBe(0)
     expect(this.teams.length).toBe(1)
   }
 
+  // 3
+  expectWithThree(){
+    expect(this.socket.emit.mock.calls.length).toBe(5)
+    expect(this.socket.emit.mock.calls)
+      .toEqual(defaultMSG_grup.concat([
+        ["msg", { "msg": "searching" }]
+      ]))
+    expect(this.searching.length).toBe(1)
+    expect(this.teams.length).toBe(1)
+  }
 
-  expectWithCancel(){
-    expect(this.socket.emit.mock.calls.length).toBe(2)
-    expect(this.socket.emit.mock.calls).toEqual([
-      ["msg", { "msg": "searching" }],
-      ["msg", { "msg": "cancel" }]
-    ])
+  // 4
+  expectWithFour(){
+    expect(this.socket.emit.mock.calls.length).toBe(8)
+    expect(this.socket.emit.mock.calls)
+      .toEqual(defaultMSG_grup.concat(defaultMSG_grup))
     expect(this.searching.length).toBe(0)
+    expect(this.teams.length).toBe(2)
   }
 }
