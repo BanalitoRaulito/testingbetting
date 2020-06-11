9f3df8b23e93776259424caa2ae661a35406713f
BanalitoRaulito <raul.volmar@gmail.com>
2020-06-12 01:47:09 +0300

    this might be a bad idea

--- src/test.js
+++ src/test.js
@@ -16,6 +16,14 @@ var makeLobby = () => {
 }
 
 
+
+var msg_searching = ["msg", { "msg": "searching" }]
+var msg_acceptNow = ["msg", { "msg": "acceptNow" }]
+var defaultMSG_grup = [
+  msg_searching,  msg_searching,
+  msg_acceptNow,  msg_acceptNow
+]
+
 // test 1
 test("matchTeam 1 player connects and then cancels", () => {
   let l = makeLobby()
@@ -27,27 +35,73 @@ test("matchTeam 1 player connects and then cancels", () => {
   l.expectWithCancel()
 })
 
-
+// msg_length, msg, searching_length, teams_length, searching = 0, teams = 0
 // test 2
+test("matchTeam 1 player joins 2x", () => {
+  let l = makeLobby()
+
+  matchTeam(l.socket, l.data, l.searching, l.teams)
+  l.expectWith(
+    1,
+    [msg_searching],
+    1,
+    0,
+    [{
+      address: "TPyeiZZ32LwQgv8AVdoq6XCpaJK8jbJ1Pj",
+      socket: l.socket,
+    }],
+    []
+  )
+
+  matchTeam(l.socket, l.data, l.searching, l.teams)
+  l.expectWith(
+    2,
+    [msg_searching, msg_searching],
+    1,
+    0,
+    [{
+      address: "TPyeiZZ32LwQgv8AVdoq6XCpaJK8jbJ1Pj",
+      socket: l.socket,
+    }],
+    []
+  )
+})
+
+
+// test 3
 test("matchTeam 4 player connect", () => {
   let l = makeLobby()
 
   // 1
   matchTeam(l.socket, l.data, l.searching, l.teams)
-  l.expectWithOne()
+  l.expectWith(
+    1,
+    [msg_searching],
+    1,
+    0,
+    [{
+      address: "TPyeiZZ32LwQgv8AVdoq6XCpaJK8jbJ1Pj",
+      socket: l.socket,
+    }],
+    []
+  )
 
   // 2
   l.data.address = "TEL1u2FV3yTh6iiPFC3KCXFzeXbnU7SxLR"
   matchTeam(l.socket, l.data, l.searching, l.teams)
-  l.expectWithTwo()
+  l.expectWith(4, defaultMSG_grup, 0, 1)
+
+  // 2 - forcing 2nd twice
+  matchTeam(l.socket, l.data, l.searching, l.teams)
+  l.expectWith(4, defaultMSG_grup, 0, 1)
 
   // 3
   l.data.address = "TBzs1vEYLkWRasgrt9r6CfzC1tvxuLv8GZ"
   matchTeam(l.socket, l.data, l.searching, l.teams)
-  l.expectWithThree()
+  l.expectWith(5, defaultMSG_grup.concat([msg_searching]), 1, 1)
 
   // 4
   l.data.address = "TNDd8X6vH16ougLhtUjPNeXPT5rkAuCp2m"
   matchTeam(l.socket, l.data, l.searching, l.teams)
-  l.expectWithFour()
+  l.expectWith(8, defaultMSG_grup.concat(defaultMSG_grup), 0, 2)
 })
--- src/test_lobby.js
+++ src/test_lobby.js
@@ -1,9 +1,4 @@
-var defaultMSG_grup = [
-  ["msg", { "msg": "searching" }],
-  ["msg", { "msg": "searching" }],
-  ["msg", { "msg": "acceptNow" }],
-  ["msg", { "msg": "acceptNow" }]
-]
+
 
 module.exports = class Lobby{
   constructor(socket, data, searching, teams){
@@ -23,44 +18,19 @@ module.exports = class Lobby{
     expect(this.searching.length).toBe(0)
   }
 
-  // 1
-  expectWithOne(){
-    expect(this.socket.emit.mock.calls.length).toBe(1)
-    expect(this.socket.emit.mock.calls[0]).toEqual([
-      "msg", { msg: "searching" }
-    ])
-    expect(this.searching).toEqual([{
-      address: "TPyeiZZ32LwQgv8AVdoq6XCpaJK8jbJ1Pj",
-      socket: this.socket,
-    }])
-    expect(this.teams).toEqual([])
-  }
 
-  // 2
-  expectWithTwo(){
-    expect(this.socket.emit.mock.calls.length).toBe(4)
-    expect(this.socket.emit.mock.calls).toEqual(defaultMSG_grup)
-    expect(this.searching.length).toBe(0)
-    expect(this.teams.length).toBe(1)
-  }
 
-  // 3
-  expectWithThree(){
-    expect(this.socket.emit.mock.calls.length).toBe(5)
-    expect(this.socket.emit.mock.calls)
-      .toEqual(defaultMSG_grup.concat([
-        ["msg", { "msg": "searching" }]
-      ]))
-    expect(this.searching.length).toBe(1)
-    expect(this.teams.length).toBe(1)
-  }
-
-  // 4
-  expectWithFour(){
-    expect(this.socket.emit.mock.calls.length).toBe(8)
-    expect(this.socket.emit.mock.calls)
-      .toEqual(defaultMSG_grup.concat(defaultMSG_grup))
-    expect(this.searching.length).toBe(0)
-    expect(this.teams.length).toBe(2)
+  expectWith(length, msg, searching_l, teams_l, searching = 0, teams = 0){
+    expect(this.socket.emit.mock.calls.length).toBe(length)
+    expect(this.socket.emit.mock.calls).toEqual(msg)
+    expect(this.searching.length).toBe(searching_l)
+    expect(this.teams.length).toBe(teams_l)
+    
+    if(searching){
+      expect(this.searching).toEqual(searching)
+    }
+    if(teams){
+      expect(this.teams).toEqual(teams)
+    }
   }
 }
